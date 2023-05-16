57d3c9ca51fe76aba1aa0ab699ad35bf85557e83
yuta3003 <y.nakazawa3003+github@gmail.com>
2023-05-16 14:23:55 +0900

    test

--- etc/scripts/mklink/mklink_x64.sh
+++ etc/scripts/mklink/mklink_x64.sh
@@ -3,14 +3,15 @@ set -eu
 
 
 CURRENT_DIR=$(cd $(dirname $0); pwd)
-DOT_DIRECTORY=$(cd ${CURRENT_DIR};cd ./../../..; pwd)
+# DOT_DIRECTORY=$(cd ${CURRENT_DIR};cd ./../../..; pwd)
+DOT_DIRECTORY=$(cd ${CURRENT_DIR};cd ./../..; pwd)
 # DOT_DIRECTORY="./../../.."
 SCRIPT_DIR="${DOT_DIRECTORY}/etc/scripts"
 MKLINK_SCRIPT_DIR="${SCRIPT_DIR}/mklink"
 
 main() {
-  # source ${MKLINK_SCRIPT_DIR}/rmlink.sh
-  source /Users/runner/work/dotfiles/dotfiles/etc/scripts/mklink/rmlink.sh
+  source ${MKLINK_SCRIPT_DIR}/rmlink.sh
+  # source /Users/runner/work/dotfiles/dotfiles/etc/scripts/mklink/rmlink.sh
 
   cd ${DOT_DIRECTORY}/git
   create_symlink git x64/.gitconfig
