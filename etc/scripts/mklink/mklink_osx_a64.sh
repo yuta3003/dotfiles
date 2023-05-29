4e249cc392c4d4c3177aced94e49bdbf76fd03c7
yuta3003 <y.nakazawa3003+github@gmail.com>
2023-05-29 13:41:28 +0900

    :+1: .gitファイルの保管場所を変更

--- etc/scripts/mklink/mklink_osx_a64.sh
+++ etc/scripts/mklink/mklink_osx_a64.sh
@@ -29,8 +29,8 @@ main() {
   source "${MKLINK_SCRIPT_DIR}/rmlink.sh"
 
   cd "${DOT_DIRECTORY}/git"
-  create_symlink git a64/.gitconfig
-  create_symlink git a64/.gitmessage
+  create_symlink git a64/config
+  create_symlink git a64/message
   create_symlink git a64/ignore
 
   cd "${DOT_DIRECTORY}/homebrew"
